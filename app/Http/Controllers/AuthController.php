013022b7f5c27bbf46ffd627775db8a68c4cd905
Dylan Du-Bois <dylan.dubois@gmail.com>
2019-10-21 22:24:12 +1100

    connected oauth register to api register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -119,17 +119,14 @@ $scopes = ['tutor'];
         //     ]
         // ]);
         $apiResponse = $client->request('POST', 'https://markit.mijdas.com/api/user/', [
-            // 'json' => [
-            //     'request' => 'SIGN_UP',
-            //     'username' => $request->get('username'),
-            //     'password' => $request->get('password'),
-            //     'email' => $request->get('email'),
-            //     'firstName' => $request->get('name'),
-            //     'lastName' => $request->get('name'),
-            //     'permissionType' => $request->get('scopes')
-            // ]
             'json' => [
-                'request' => 'SIGN_UP'
+                'request' => 'SIGN_UP',
+                'username' => $request->get('username'),
+                'password' => $request->get('password'),
+                'email' => $request->get('email'),
+                'firstName' => $request->get('name'),
+                'lastName' => $request->get('name'),
+                'permissionType' => $request->get('scopes')
             ]
         ]);
 
